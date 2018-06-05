197cf0f93087c90e83dabe007f35d2b7438f9937
jason03171 <jason0317@kimo.com>
2018-06-05 11:54:18 +0800

    將form3的listbox改成panel

--- WindowsFormsApplication1/Form3.Designer.cs
+++ WindowsFormsApplication1/Form3.Designer.cs
@@ -28,30 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
             this.users = new System.Windows.Forms.Label();
             this.new_item = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(17, 138);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(455, 388);
-            this.listBox1.TabIndex = 0;
             // 
             // vScrollBar1
             // 
-            this.vScrollBar1.Location = new System.Drawing.Point(452, 138);
+            this.vScrollBar1.Location = new System.Drawing.Point(586, 0);
             this.vScrollBar1.Name = "vScrollBar1";
-            this.vScrollBar1.Size = new System.Drawing.Size(20, 388);
+            this.vScrollBar1.Size = new System.Drawing.Size(20, 485);
             this.vScrollBar1.TabIndex = 1;
             this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
             // 
@@ -59,9 +51,10 @@
             // 
             this.users.AutoSize = true;
             this.users.Font = new System.Drawing.Font("微軟正黑體", 16F);
-            this.users.Location = new System.Drawing.Point(98, 64);
+            this.users.Location = new System.Drawing.Point(131, 80);
+            this.users.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.users.Name = "users";
-            this.users.Size = new System.Drawing.Size(112, 28);
+            this.users.Size = new System.Drawing.Size(139, 34);
             this.users.TabIndex = 11;
             this.users.Text = "username";
             // 
@@ -70,9 +63,10 @@
             this.new_item.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
             this.new_item.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.new_item.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.new_item.Location = new System.Drawing.Point(370, 62);
+            this.new_item.Location = new System.Drawing.Point(493, 78);
+            this.new_item.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.new_item.Name = "new_item";
-            this.new_item.Size = new System.Drawing.Size(102, 34);
+            this.new_item.Size = new System.Drawing.Size(136, 42);
             this.new_item.TabIndex = 12;
             this.new_item.Text = "+ 新增商品";
             this.new_item.UseVisualStyleBackColor = false;
@@ -83,18 +77,20 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label1.ForeColor = System.Drawing.Color.Navy;
-            this.label1.Location = new System.Drawing.Point(12, 107);
+            this.label1.Location = new System.Drawing.Point(16, 134);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 31);
+            this.label1.Size = new System.Drawing.Size(137, 38);
             this.label1.TabIndex = 13;
             this.label1.Text = "銷售物品";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::WindowsFormsApplication1.Properties.Resources.face_photo;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(16, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox1.Size = new System.Drawing.Size(107, 100);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
@@ -102,42 +98,52 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("微軟正黑體", 17.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label5.Location = new System.Drawing.Point(7, 73);
+            this.label5.Location = new System.Drawing.Point(9, 91);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(473, 29);
+            this.label5.Size = new System.Drawing.Size(615, 37);
             this.label5.TabIndex = 23;
             this.label5.Text = "______________________________________________";
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.vScrollBar1);
+            this.panel1.Location = new System.Drawing.Point(23, 176);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(606, 481);
+            this.panel1.TabIndex = 24;
+            // 
             // Form3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.ClientSize = new System.Drawing.Size(484, 611);
+            this.ClientSize = new System.Drawing.Size(645, 764);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.new_item);
             this.Controls.Add(this.users);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.vScrollBar1);
-            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label5);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form3";
             this.Text = "Form3";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.VScrollBar vScrollBar1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button new_item;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label5;
         public System.Windows.Forms.Label users;
+        private System.Windows.Forms.Panel panel1;
     }
 }
