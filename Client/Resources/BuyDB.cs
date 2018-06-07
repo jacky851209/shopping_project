a098e7067c097f11e5841d89f25107d6e9f7285a
jason03171 <jason0317@kimo.com>
2018-06-07 22:05:19 +0800

    新增buyDB function

--- Client/Resources/BuyDB.cs
+++ Client/Resources/BuyDB.cs
@@ -10,14 +10,14 @@ using MongoDB.Driver.GridFS;
 
 namespace WindowsFormsApplication1.Resources
 {
-    
+
     class BuyDB
     {
         private MongoClient _mongoClient;
         private MongoServer _mongoServer;
         private MongoDatabase _mongoDatabase;
         private MongoCollection<Buy> _mongoCollection1;
-        
+
         public BuyDB()
         {
             // MongoDB 連線字串
@@ -32,10 +32,35 @@ namespace WindowsFormsApplication1.Resources
             _mongoCollection1 = _mongoDatabase.GetCollection<Buy>("buy");
         }
 
-        public void add_buy(String buyeremail,String item,int price,int count)
+        public void add_buy(String buyeremail, String item, int price, int count)
         {
             var coll = _mongoDatabase.GetCollection<Buy>("buy");
-            coll.Insert(new BsonDocument { { "Buyeremail", buyeremail }, { "Productname", item },{ "Price", price }, { "Count", count }});
+            coll.Insert(new BsonDocument { { "Buyeremail", buyeremail }, { "Productname", item }, { "Price", price }, { "Count", count } });
+        }
+
+        public int order_count(String email)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("shopping");
+            var collection = database.GetCollection<Buy>("buy");
+
+
+            var filter = Builders<Buy>.Filter.Eq(x => x.BuyerEmail, email);
+            var results = collection.Find(filter).Count();
+
+            return (int)results;
+        }
+
+        public async Task<List<Buy>> get_order(String email)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("shopping");
+            var collection = database.GetCollection<Buy>("buy");
+
+            var filter = Builders<Buy>.Filter.Eq(x => x.BuyerEmail, email);
+            var list = await collection.Find(filter).ToListAsync();
+
+            return list;
         }
     }
 }
